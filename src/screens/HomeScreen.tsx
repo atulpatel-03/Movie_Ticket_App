d8239ea4398729ba5ec1fbd5b55d33026935a65d
atulpatel-03 <atulpatel22777@gmail.com>
2023-09-19 10:53:32 +0530

    feat: home-page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import {
   Text,
   View,
@@ -22,12 +22,13 @@ import SearchBar from '../components/SearchBar';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import CategoryHeader from '../components/CategoryHeader';
 import SubMovieCard from '../components/SubMovieCard';
+import MovieCard from '../components/MovieCard';
 
 interface Props {}
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    flex: 1,
     backgroundColor: COLORS.Black,
   },
   loading_indicator_container: {
@@ -35,12 +36,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  scroll_view_container: {
-    flex: 1,
-  },
+
   search_bar_input: {
     marginHorizontal: SPACING.space_36,
     marginTop: SPACING.space_36,
+    marginBottom: SPACING.space_18,
   },
   flatlist_container: {
     gap: SPACING.space_28,
@@ -100,7 +100,11 @@ const HomeScreen = ({navigation}: any) => {
       set_upcoming_movies_list(upcoming);
 
       const nowplaying = await now_playing_movies_fun();
-      set_now_playing_movies_list(nowplaying);
+      set_now_playing_movies_list([
+        {id: 'dummy1'},
+        ...nowplaying,
+        {id: 'dummy2'},
+      ]);
 
       const popular = await popular_movies_fun();
       set_popular_movies_list(popular);
@@ -109,14 +113,15 @@ const HomeScreen = ({navigation}: any) => {
     set_is_loading(false);
   }, []);
 
-  console.log(upcoming_movies_list);
+  const handle_selected_movie = (id: number) => {
+    navigation.push('MovieDetail', {
+      movie_id: id,
+    });
+  };
 
   if (is_loading) {
     return (
-      <ScrollView
-        style={styles.container}
-        bounces={false}
-        contentContainerStyle={styles.scroll_view_container}>
+      <ScrollView style={styles.container} bounces={false}>
         <StatusBar hidden />
         <View style={styles.search_bar_input}>
           <SearchBar search_functionality={search_functionality} />
@@ -130,37 +135,82 @@ const HomeScreen = ({navigation}: any) => {
   }
 
   return (
-    <ScrollView
-      style={styles.container}
-      bounces={false}
-      contentContainerStyle={styles.scroll_view_container}>
+    <ScrollView style={styles.container} bounces={false}>
       <StatusBar hidden />
       <View style={styles.search_bar_input}>
         <SearchBar search_functionality={search_functionality} />
       </View>
       <CategoryHeader>Now Playing</CategoryHeader>
+      <FlatList
+        horizontal
+        bounces={false}
+        data={now_playing_movies_list}
+        keyExtractor={item => item.id}
+        contentContainerStyle={[styles.flatlist_container]}
+        snapToInterval={width * 0.7 + SPACING.space_4}
+        decelerationRate={0}
+        renderItem={({item, index}) => {
+          if (!item.original_title) {
+            return (
+              <View
+                style={{
+                  width: (width - (width * 0.7 + SPACING.space_36 * 2)) / 2,
+                }}
+              />
+            );
+          }
+          return (
+            <MovieCard
+              card_function={handle_selected_movie}
+              card_width={width * 0.7}
+              title={item.original_title}
+              image_link={base_image_path('w780', item.poster_path)}
+              movie_id={item.id}
+              vote_average={item.vote_average}
+              vote_count={item.vote_count}
+              genre_ids={item.genre_ids.slice(1, 4)}
+            />
+          );
+        }}
+      />
       <CategoryHeader>Popular</CategoryHeader>
       <FlatList
         horizontal
+        bounces={false}
         data={popular_movies_list}
         keyExtractor={item => item.id}
         contentContainerStyle={styles.flatlist_container}
         renderItem={({item, index}) => {
           return (
             <SubMovieCard
-              cardFunction={() => {
-                navigation.push('MovieDetail', {
-                  movie_id: item.id,
-                });
-              }}
+              card_function={handle_selected_movie}
               card_width={width / 3}
               title={item.original_title}
               image_link={base_image_path('w342', item.poster_path)}
+              movie_id={item.id}
             />
           );
         }}
       />
       <CategoryHeader>Upcoming</CategoryHeader>
+      <FlatList
+        horizontal
+        bounces={false}
+        data={upcoming_movies_list}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.flatlist_container}
+        renderItem={({item, index}) => {
+          return (
+            <SubMovieCard
+              card_function={handle_selected_movie}
+              card_width={width / 3}
+              title={item.original_title}
+              image_link={base_image_path('w342', item.poster_path)}
+              movie_id={item.id}
+            />
+          );
+        }}
+      />
     </ScrollView>
   );
 };
